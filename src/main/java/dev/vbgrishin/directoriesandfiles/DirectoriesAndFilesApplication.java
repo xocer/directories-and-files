5c0d885f4d37446ae0d124568c0c4a53f4606144
viktor.b.grishin <viktor.b.grishin@nw.rt.ru>
2022-06-05 23:21:07 +0300

    init commit

---
+++ src/main/java/dev/vbgrishin/directoriesandfiles/DirectoriesAndFilesApplication.java
@@ -0,0 +1,13 @@
+package dev.vbgrishin.directoriesandfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoriesAndFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectoriesAndFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vbgrishin/directoriesandfiles/DirectoriesAndFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vbgrishin.directoriesandfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectoriesAndFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
